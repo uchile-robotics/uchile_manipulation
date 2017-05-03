6f2eb4646904b946bfd6b88d117cbd4cf26b5e42
Giovanni Pais <gio.pais@hotmail.com>
2017-05-03 06:00:28 -0300

    Se crea package bender_moveit_python donde se modifican clases para bender. Se completa un poco mas el new_bender_planning

--- bender_arm_control/src/bender_arm_control/new_bender_planning.py
+++ bender_arm_control/src/bender_arm_control/new_bender_planning.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 import rospy
 
+from bender_moveit_python import move_group_interface
+
+JOINT_STATE_TOPIC = "/bender/joint_states"
+ROBOT_DESCRIPTION = "robot_description"
+MONITORED_PLANNING_SCENE_TOPIC = "/move_group/monitored_planning_scene"
+ATTACHED_COLLISION_OBJECT_TOPIC = "/attached_collision_object"
+COLLISION_OBJECT_TOPIC = "/collision_object"
+PLANNING_SCENE_TOPIC = "/planning_scene"
+PLANNING_SCENE_WORLD_TOPIC = "/planning_scene_world"
+
 def main():
 
     #Iniciar Nodo ROS
     rospy.init_node('new_bender_planning')
 
-    #Obtener argumentos
+    #Obtener argumentos (POR AHORA PARA l_arm)
+    planning_group ="l_arm"
 
     #Cargar planning scene monitor
 
     #Modificar planning scene monitor para obtener el estado del robot
+
+    rospy.loginfo("Publishing planning scene on "+PLANNING_SCENE_TOPIC)
 
     #Interface para move_group l_arm y r_arm
 
