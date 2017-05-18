5d0024e7e0cda7ac04808b9b828f170c94fcd5e9
Giovanni Pais <gio.pais@hotmail.com>
2017-05-18 16:03:10 -0400

    Se agregan test para probar nuevas funciones de moveitpython. test 4 funciona en el simulador una sola vez y luego gripper siempre tiene colision. Uso de no-verify me reclama por import pero este funciona

---
+++ bender_arm_control/scripts/test/test4.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+__author__ = 'Giovanni Pais'
+__email__ = 'giom.pais@gmail.com'
+
+import rospy
+from bender_arm_control.arm_commander import Limb
+from moveit_python import *
+
+
+
+
+def main():
+    
+    
+    g = MoveGroupInterface("l_arm", "bender/l_arm_base",None,False)
+
+    joint_names=['l_shoulder_pitch_joint', 'l_shoulder_roll_joint',
+      'l_shoulder_yaw_joint', 'l_elbow_pitch_joint', 'l_elbow_yaw_joint',
+      'l_wrist_pitch_joint']
+    
+    print "INICIA TEST"
+    home =[0.0,0.0,0.0,0.0,0.0,0.0]
+    pos1=[0.373582124710083, 0.4276796877384186, 0.5884131789207458, 1.1624618768692017, -0.5723373293876648, 0.25651833415031433]
+    pos2=[0.373582124710083, 0.4276796877384186, 0.5884131789207458, 1.7700000000000017, -0.5723373293876648, 0.25651833415031433]
+
+
+    print "HOMING"
+
+    g.moveToJointPosition(joint_names,pos1)
+    rospy.sleep(2.0)
+
+    print "POSITION: HOME"
+
+    rospy.sleep(1.0)
+
+
+    
+    # print "pos1 START"
+    # limb.arm.move_joint(pos1)
+    # rospy.sleep(5.0)
+    # print "pos2 START"
+    # limb.arm.move_joint(pos2)
+    # rospy.sleep(5.0)
+    # print "pos2 END"
+    print "TERMINA TEST"
+    print "BYE BYE =)"
+    ################################
+
+
+if __name__ == '__main__':
+    rospy.init_node('planning_tester')
+    main()
