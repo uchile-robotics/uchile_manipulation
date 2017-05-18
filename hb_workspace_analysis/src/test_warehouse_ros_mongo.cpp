bfb10c15a198f0e29948f54aff2afdce69e98a93
Rodrigo Munoz <rorro.mr@gmail.com>
2017-05-17 22:11:23 -0400

    Fix drop database

--- hb_workspace_analysis/src/test_warehouse_ros_mongo.cpp
+++ hb_workspace_analysis/src/test_warehouse_ros_mongo.cpp
@@ -42,94 +42,94 @@ void test()
   mongo_ros::dropDatabase("capability_map", "localhost", 27017, 5.0);
   
   // Set up db
-  mongo_ros::MessageCollection<hb_workspace_analysis::GraspStorage> coll("workspace_analysis", "capability_map", "localhost",
+  mongo_ros::MessageCollection<hb_workspace_analysis::GraspStorage> coll("workspace_analysis", "localhost",
                                        27017, 5.0);
 
   // Arrange to index on metadata field 'z'
   coll.ensureIndex("z");
 
-    // Get arm info from param server
-    ros::NodeHandle nh_("~");
-    std::string planning_group_name_;
-    nh_.param("arm", planning_group_name_, std::string("l_arm"));
-    std::string ee_group_name_;
-    nh_.param("ee_name", ee_group_name_, std::string("l_gripper"));
+  // Get arm info from param server
+  ros::NodeHandle nh_("~");
+  std::string planning_group_name_;
+  nh_.param("arm", planning_group_name_, std::string("l_arm"));
+  std::string ee_group_name_;
+  nh_.param("ee_name", ee_group_name_, std::string("l_gripper"));
 
-    ROS_INFO_STREAM_NAMED("test","Planning Group: " << planning_group_name_);
-    ROS_INFO_STREAM_NAMED("test","End Effector: " << ee_group_name_);
-
-
-    // ---------------------------------------------------------------------------------------------
-    // Load planning scene to share
-    planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor_(new planning_scene_monitor::PlanningSceneMonitor("robot_description"));
-
-    // Get joint model groups
-    const robot_model::JointModelGroup
-            *ee_jmg = planning_scene_monitor_->getRobotModel()->getJointModelGroup(ee_group_name_);
-    const robot_model::JointModelGroup
-            *arm_jmg = planning_scene_monitor_->getRobotModel()->getJointModelGroup(planning_group_name_);
-
-    // ---------------------------------------------------------------------------------------------
-    // Load grasp options
-    hb_grasp_generator::GraspOptions opt;
-    opt.load(nh_, ee_group_name_);
-    ros::NodeHandle grasp_nh(nh_, ee_group_name_);
-    hb_grasp_generator::CylindricalGraspGeneratorPtr simple_grasps_(new hb_grasp_generator::CylindricalGraspGenerator(grasp_nh, opt));
+  ROS_INFO_STREAM_NAMED("test","Planning Group: " << planning_group_name_);
+  ROS_INFO_STREAM_NAMED("test","End Effector: " << ee_group_name_);
 
 
-    // ---------------------------------------------------------------------------------------------
-    // Load Grasp filter
-    const robot_state::RobotState& robot_state = planning_scene_monitor_->getPlanningScene()->getCurrentState();
-    hb_grasp_generator::GraspFilterPtr grasp_filter_(new hb_grasp_generator::GraspFilter(robot_state) );
+  // ---------------------------------------------------------------------------------------------
+  // Load planning scene to share
+  planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor_(new planning_scene_monitor::PlanningSceneMonitor("robot_description"));
 
-    // ---------------------------------------------------------------------------------------------
-    // Test grasp position
-    geometry_msgs::Pose object_pose;
-    object_pose.position.x = 0.50;
-    object_pose.position.y = 0.29;
-    object_pose.position.z = 0.60;
+  // Get joint model groups
+  const robot_model::JointModelGroup
+          *ee_jmg = planning_scene_monitor_->getRobotModel()->getJointModelGroup(ee_group_name_);
+  const robot_model::JointModelGroup
+          *arm_jmg = planning_scene_monitor_->getRobotModel()->getJointModelGroup(planning_group_name_);
 
-    object_pose.orientation.x = 0.0;
-    object_pose.orientation.y = 0.0;
-    object_pose.orientation.z = 0.0;
-    object_pose.orientation.w = 1.0;
+  // ---------------------------------------------------------------------------------------------
+  // Load grasp options
+  hb_grasp_generator::GraspOptions opt;
+  opt.load(nh_, ee_group_name_);
+  ros::NodeHandle grasp_nh(nh_, ee_group_name_);
+  hb_grasp_generator::CylindricalGraspGeneratorPtr simple_grasps_(new hb_grasp_generator::CylindricalGraspGenerator(grasp_nh, opt));
 
-    std::vector<moveit_msgs::Grasp> possible_grasps;
-    std::vector<trajectory_msgs::JointTrajectoryPoint> ik_solutions; // save each grasps ik solution for visualization
 
-    ROS_INFO_STREAM_NAMED("test","Adding object");
+  // ---------------------------------------------------------------------------------------------
+  // Load Grasp filter
+  const robot_state::RobotState& robot_state = planning_scene_monitor_->getPlanningScene()->getCurrentState();
+  hb_grasp_generator::GraspFilterPtr grasp_filter_(new hb_grasp_generator::GraspFilter(robot_state) );
 
-    possible_grasps.clear();
-    ik_solutions.clear();
+  // ---------------------------------------------------------------------------------------------
+  // Test grasp position
+  geometry_msgs::Pose object_pose;
+  object_pose.position.x = 0.50;
+  object_pose.position.y = 0.29;
+  object_pose.position.z = 0.60;
 
-    // Generate set of grasps for one object
-    simple_grasps_->generateGrasp(object_pose, possible_grasps);
+  object_pose.orientation.x = 0.0;
+  object_pose.orientation.y = 0.0;
+  object_pose.orientation.z = 0.0;
+  object_pose.orientation.w = 1.0;
 
-    // Apply grasp filter
-    bool filter_pregrasps = true;
-    double ik_timeout = 0.1;
-    grasp_filter_->filterGrasps(possible_grasps,
-                                ik_solutions,
-                                filter_pregrasps,
-                                opt.end_effector_parent_link,
-                                planning_group_name_, ik_timeout);
+  std::vector<moveit_msgs::Grasp> possible_grasps;
+  std::vector<trajectory_msgs::JointTrajectoryPoint> ik_solutions; // save each grasps ik solution for visualization
 
-    // Construct grasp storage
-    hb_workspace_analysis::GraspStorage grasp_vector;
-    grasp_vector.header.frame_id = opt.base_link;
-    grasp_vector.pose = object_pose;
-    grasp_vector.grasp.reserve(possible_grasps.size());
-    for(std::size_t i = 0; i < possible_grasps.size(); ++i)
-    {
-        hb_workspace_analysis::GraspPoint point;
-        point.grasp = possible_grasps[i];
-        point.pregrasp_position = ik_solutions[2*i];
-        point.grasp_position = ik_solutions[2*i+1];
-        grasp_vector.grasp.push_back(point);
-    }
+  ROS_INFO_STREAM_NAMED("test", "Adding object");
 
-    coll.insert(grasp_vector, makeMetadata(grasp_vector));
-    ROS_INFO_STREAM("Collection count: " << coll.count());
+  possible_grasps.clear();
+  ik_solutions.clear();
+
+  // Generate set of grasps for one object
+  simple_grasps_->generateGrasp(object_pose, possible_grasps);
+
+  // Apply grasp filter
+  bool filter_pregrasps = true;
+  double ik_timeout = 0.1;
+  grasp_filter_->filterGrasps(possible_grasps,
+                              ik_solutions,
+                              filter_pregrasps,
+                              opt.end_effector_parent_link,
+                              planning_group_name_, ik_timeout);
+
+  // Construct grasp storage
+  hb_workspace_analysis::GraspStorage grasp_vector;
+  grasp_vector.header.frame_id = opt.base_link;
+  grasp_vector.pose = object_pose;
+  grasp_vector.grasp.reserve(possible_grasps.size());
+  for(std::size_t i = 0; i < possible_grasps.size(); ++i)
+  {
+      hb_workspace_analysis::GraspPoint point;
+      point.grasp = possible_grasps[i];
+      point.pregrasp_position = ik_solutions[2*i];
+      point.grasp_position = ik_solutions[2*i+1];
+      grasp_vector.grasp.push_back(point);
+  }
+
+  coll.insert(grasp_vector, makeMetadata(grasp_vector));
+  ROS_INFO_STREAM("Collection count: " << coll.count());
 
   // Add some poses and metadata
 /*  const gm::Pose p1 = makePose(24, 42, 0);
