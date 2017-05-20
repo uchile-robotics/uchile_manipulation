3c17aee6f02fac19e2deb3e506db5e543228137e
Giovanni Pais <gio.pais@hotmail.com>
2017-05-19 22:12:21 -0400

    Se trata de hacer test para otra funcion de las skills. IMCOMPLETA se hace commit para trabajar en maquinas de estado

--- bender_arm_control/scripts/test/test4.py
+++ bender_arm_control/scripts/test/test4.py
@@ -9,6 +9,9 @@ import rospy
 from bender_arm_control.arm_commander import Limb
 from moveit_python import *
 
+from geometry_msgs.msg import Pose, PoseStamped, Point
+
+
 
 
 
@@ -25,11 +28,22 @@ def main():
     home =[0.0,0.0,0.0,0.0,0.0,0.0]
     pos1=[0.373582124710083, 0.4276796877384186, 0.5884131789207458, 1.1624618768692017, -0.5723373293876648, 0.25651833415031433]
     pos2=[0.373582124710083, 0.4276796877384186, 0.5884131789207458, 1.7700000000000017, -0.5723373293876648, 0.25651833415031433]
-
+    spos=PoseStamped()
+    spos.pose.position.x=0.60
+    spos.pose.position.x=0.25
+    spos.pose.position.x=0.75
+    
 
     print "HOMING"
 
+
+    #Movimiento en espacio de joint
     g.moveToJointPosition(joint_names,pos1)
+
+
+    #Movimiento en espacio cartesiano
+    #g.moveToPose(spos,"bender/l_wrist_pitch_link")
+
     rospy.sleep(2.0)
 
     print "POSITION: HOME"
