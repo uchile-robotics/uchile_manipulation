31d3d95dc0b7608db61904fb6c9e63ad7ea4319e
Giovanni Pais <gio.pais@hotmail.com>
2017-06-01 17:13:31 -0400

    tests version 2.0

--- bender_arm_control/scripts/test/named_pos_test.py
+++ bender_arm_control/scripts/test/named_pos_test.py
@@ -18,26 +18,22 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 def main():
     
     
-    g = MoveGroupInterface("l_arm", "bender/l_shoulder_pitch_link",None,False)
+    g = MoveGroupInterface("l_arm", "bender/base_link",None,False)
 
     joint_names=['l_shoulder_pitch_joint', 'l_shoulder_roll_joint',
       'l_shoulder_yaw_joint', 'l_elbow_pitch_joint', 'l_elbow_yaw_joint',
       'l_wrist_pitch_joint']
     
-    print "INICIA TEST"
+    rospy.loginfo('INICIA TEST')
+
     l_arm_home =[0.0,0.0,0.0,0.0,0.0,0.0]
     l_arm_middle=[1.8, 0.0, -0.35, 0.1, 0.0, 0.4]
-
-    #l_arm_premanip_1=[1.674, -0.041, -0.89, 0.129, -0.066, 1.053]
-    #l_arm_premanip_2=[1.782, -0.031, -0.23, 0.129, -0.102, 0.997]
-    
-    #Prueba
     l_arm_premanip_1=[-0.89, 0.129, -0.066, 1.674, -0.041, 1.053]
     l_arm_premanip_2=[-0.23, 0.129, -0.102, 1.782, -0.031, 0.997]
     
 
     #INSERTAR NOMBRE DE POSICION
-    named_position="premanip_1"
+    named_position="sdasd"
     #############################################################
     named_angles=[0.0,0.0,0.0,0.0,0.0,0.0]
     #solucion temporal, estoy buscando como lo implementa el movegroup de cpp para hacer lo mismo y sacarlo del SRDF
@@ -54,28 +50,20 @@ def main():
     				named_angles=l_arm_premanip_2
     			else:
     				named_position="home"
-    				print "ERROR:Posicion desconocida"
+    				rospy.logerr('ERROR! Unknown position')
     
     #############################################################
 
     #g.moveToJointPosition(joint_names,l_arm_home)
     #rospy.sleep(2.0)
-    #print "POSITION: HOME"
     rospy.sleep(1.0)
     g.moveToJointPosition(joint_names,named_angles)
-    rospy.sleep(3.0)
-    print "POSITION: " + named_position
+    rospy.sleep(2.0)
+    pos = "POSITION: " + named_position
+    rospy.loginfo(pos)
 
-    
-    # print "pos1 START"
-    # limb.arm.move_joint(pos1)
-    # rospy.sleep(5.0)
-    # print "pos2 START"
-    # limb.arm.move_joint(pos2)
-    # rospy.sleep(5.0)
-    # print "pos2 END"
-    print "TERMINA TEST"
-    print "BYE BYE =)"
+    rospy.sleep(1.0)
+    rospy.loginfo('TERMINA TEST')
     ################################
 
 
--- bender_arm_control/scripts/test/set_joint_test.py
+++ bender_arm_control/scripts/test/set_joint_test.py
@@ -18,32 +18,35 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 def main():
     
     
-    g = MoveGroupInterface("l_arm", "bender/l_shoulder_pitch_link",None,False)
+    g = MoveGroupInterface("l_arm", "bender/base_link",None,False)
 
     joint_names=['l_shoulder_pitch_joint', 'l_shoulder_roll_joint',
       'l_shoulder_yaw_joint', 'l_elbow_pitch_joint', 'l_elbow_yaw_joint',
       'l_wrist_pitch_joint']
     
-    print "INICIA TEST"
+    rospy.loginfo('INICIA TEST')
     home =[0.0,0.0,0.0,0.0,0.0,0.0]
-    pos1=[0.373582124710083, 0.4276796877384186, 0.5884131789207458, 1.1624618768692017, -0.5723373293876648, 0.25651833415031433]
-    pos2=[0.373582124710083, 0.4276796877384186, 0.5884131789207458, 1.7700000000000017, -0.5723373293876648, 0.25651833415031433]
+    pos_1=[0.373582124710083, 0.4276796877384186, 0.5884131789207458, 1.1624618768692017, -0.5723373293876648, 0.25651833415031433]
+    pos_2=[0.373582124710083, 0.4276796877384186, 0.5884131789207458, 1.7700000000000017, -0.5723373293876648, 0.25651833415031433]
+    l_arm_premanip_1=[-0.89, 0.129, -0.066, 1.674, -0.041, 1.053]
     
     
 
     #Movimiento en espacio de joint
     g.moveToJointPosition(joint_names,home)
     rospy.sleep(2.0)
-    print "POSITION: HOME"
+    rospy.loginfo('POSITION: HOME')
     rospy.sleep(1.0)
-    #g.moveToJointPosition(joint_names,pos1)
-    rospy.sleep(3.0)
-    print "POSITION: pos1"
-    #g.moveToJointPosition(joint_names,pos2)
-    rospy.sleep(3.0)
-    print "POSITION: pos2"
-    print "TERMINA TEST"
-    print "BYE BYE =)"
+    g.moveToJointPosition(joint_names,l_arm_premanip_1)
+    rospy.sleep(2.0)
+    rospy.loginfo('POSITION: l_arm_premanip_1')
+    g.moveToJointPosition(joint_names,pos_1)
+    rospy.sleep(2.0)
+    rospy.loginfo('POSITION: pos_1')
+    g.moveToJointPosition(joint_names,pos_2)
+    rospy.sleep(2.0)
+    rospy.loginfo('POSITION: pos_2')
+    rospy.loginfo('TERMINA TEST')
     ################################
 
 
