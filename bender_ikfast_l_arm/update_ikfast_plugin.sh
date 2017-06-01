968de57e83bb284809855ce60c3ad9ac7fd89446
Matías Pavez <matias.pavez.b@gmail.com>
2017-06-01 15:43:48 -0400

    migrating missing changes after merge revert on bitbucket

--- bender_ikfast_l_arm/update_ikfast_plugin.sh
+++ bender_ikfast_l_arm/update_ikfast_plugin.sh
@@ -1,1 +1,2 @@
-rosrun moveit_ikfast create_ikfast_moveit_plugin.py bender l_arm bender_ikfast_l_arm /home/rodrigo/bender_ws/soft_ws/src/bender_manipulation/bender_ikfast_l_arm/src/bender_l_arm_ikfast_solver.cpp
+#!/bin/bash
+rosrun moveit_ikfast create_ikfast_moveit_plugin.py bender l_arm bender_ikfast_l_arm /home/rodrigo/bender_ws/soft_ws/src/uchile_manipulation/bender_ikfast_l_arm/src/bender_l_arm_ikfast_solver.cpp
